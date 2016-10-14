aeaa689e24fbe7f66285c28910d6ef7b99e75942
Shaun Kavanagh <shaunkavanaghwork@gmail.com>
2016-10-14 13:00:52 +0100

    Add files via upload
    so place the .xml files in your layout folder. then place the java files in app  src main java.

---
+++ Entries.java
@@ -0,0 +1,18 @@
+package com.example.shaun.securityapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by shaun on 14/10/2016.
+ */
+public class Entries extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //this setcontentView sets what xml file is being used here
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
