f3c0ad365691cdfa960495bb6d32bbbb4372e74f
Shaun Kavanagh <shaunkavanaghwork@gmail.com>
2016-12-22 12:12:19 +0000

    Update Entry.java

--- Entry.java
+++ Entry.java
@@ -22,11 +22,12 @@ import java.util.Map;
  */
 public class Entry extends AppCompatActivity  {
     String name,pass,web;
-    DatabaseReference mRootRef= FirebaseDatabase.getInstance().getReference();
+  /*  DatabaseReference mRootRef= FirebaseDatabase.getInstance().getReference();
     DatabaseReference mPostRef=mRootRef.child("UserEntriesInfo"); //to push data using this node path
     DatabaseReference mConditionRef=mRootRef.child("username");
     DatabaseReference mConditionRef1=mRootRef.child("password");
-    DatabaseReference mConditionRef2=mRootRef.child("website_entry");
+    DatabaseReference mConditionRef2=mRootRef.child("website_entry");*/
+    //this gets the reference to the database
     FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
     FirebaseUser user = firebaseAuth.getCurrentUser();
 
@@ -37,22 +38,26 @@ public class Entry extends AppCompatActivity  {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Firebase.setAndroidContext(this);
-
-
         super.onCreate(savedInstanceState);
         //this setcontentView sets what xml file is being used here
         setContentView(R.layout.activity_entry);
         Intent intent = getIntent();
+        //getting extras gets the NumID that was sent in from Entries
         Bundle extras = intent.getExtras();
         //  final String Profile =  extras.getString("filename");
         final String numID=extras.getString("NumID");
+        //init all the buttons and text fields
         final EditText passWord=(EditText)findViewById(R.id.password);
         final EditText userName=(EditText)findViewById(R.id.username);
         final EditText siteName=(EditText)findViewById(R.id.siteName);
         final Button firebaseclass= (Button)findViewById(R.id.firebaseTest);
         final Context context = this.getApplicationContext();
+        //gets the UserID
         String temp =user.getUid();
         final DatabaseReference ref = database.getReference("/Entries");
+        
+        //this commented code was supposed to call to the database when the user enters, in case they are simply editing the info
+        //we had issues with decryption so it was not used
       /*  ref.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
@@ -88,18 +93,21 @@ public class Entry extends AppCompatActivity  {
 
             }
         });*/
-
+        //when the user hits send
         firebaseclass.setOnClickListener(new Button.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //get the password they put in
                 pass=passWord.getText().toString();
-
+                //userRef sets the working path within the database
                 DatabaseReference usersRef = ref.child(user.getUid().toString());
 
                 Map<String, User> users = new HashMap<String, User>();
                 //siteName.getText().toString()
                 //usersRef.child(numID).setValue(new User(userName.getText().toString(),passWord.getText().toString(),siteName.getText().toString()))
                 //create objects
+                //this was where we were supposed to encrypt the data but unfortunately we couldnt figure out a way to send the secret key to the
+                //database without loosing any information from it because it was of type SecretKey
                 obj enc=new obj();
                 SecretKey SecKey = null;
                 //Generation of Secret key, In try catch to get rid of exceptions
@@ -116,9 +124,9 @@ public class Entry extends AppCompatActivity  {
                 }
                 byte[] sec1=SecKey.getEncoded();
                 String sec = new String(sec1);
-
+                //this is where the information is sent to the database
                 usersRef.child(numID).setValue(new User(userName.getText().toString(),pass,siteName.getText().toString() ));
-
+                //the rest of this code just sets the edit text fields to null so that the user knows their informastion has gone
                 name=userName.getText().toString();
                 //mConditionRef.setValue(userNameInput.getText());
                 // mConditionTextView.setText("*USERNAME ready to push to FIREBASE!*.");
@@ -130,7 +138,7 @@ public class Entry extends AppCompatActivity  {
              //   String Name = siteName.getText().toString()+ "\n";
                 siteName.setText("");
 
-
+                //the activity is then sent back to entries
                 Intent ButtonIntent = new Intent(Entry.this, Entries.class);
                 startActivity(ButtonIntent);
             }
