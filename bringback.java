b94734ea16b885f89685058350e451d69667b74c
Shaun Kavanagh <shaunkavanaghwork@gmail.com>
2016-12-22 12:24:21 +0000

    Update bringback.java

--- bringback.java
+++ bringback.java
@@ -2,8 +2,10 @@ package com.example.dave.test;
 
 /**
  * Created by shaun on 07/12/2016.
+ * this method declares the necessary getter methods so that the datasnapshot can be handled and the information extracted.
  */
 public class bringback{
+   
     private String Username;
     private String Password;
     private String Website;
